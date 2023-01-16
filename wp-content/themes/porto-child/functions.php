ac489a09dfc48c080baf614684ed9b00f4cb244c
Dusan Kuzmanovic <73176332+fws-dusan@users.noreply.github.com>
2023-01-16 12:37:50 +0100

    added pardot script

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -226,3 +226,21 @@ add_action(
     'woocommerce_template_single_add_to_cart',
     15
 );
+
+//Add pardot script to footer
+add_action( 'wp_footer', function() { ?>
+    <script type="text/javascript">
+     piAId = '1008492';
+     piCId = '78812';
+     piHostname = 'pi.pardot.com';
+     (function() {
+                   function async_load(){
+                                var s = document.createElement('script'); s.type = 'text/javascript';
+                                s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+                                var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+                   }
+                   if(window.attachEvent) { window.attachEvent('onload', async_load); }
+                   else { window.addEventListener('load', async_load, false); }
+     })();
+     </script>
+ <?php } );
